b090f7fa7df9ed12eb86c2aa3a0eb5f8d01372a2
alex <alex@Jennys-MacBook-Air.local>
2023-11-01 14:20:17 +0100

    Initial commit

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>DashboardPAge</div>;
+};
+
+export default page;
---
+++ app/ui/fonts.ts
@@ -0,0 +1,18 @@
+import { Inter, Montserrat, Oswald, Playpen_Sans, Lusitana } from 'next/font/google'
+
+export const inter = Inter({
+    subsets: ['latin']
+})
+export const montseratt = Montserrat({
+    subsets: ['latin']
+})
+export const oswald = Oswald({
+    subsets: ['latin']
+})
+export const playpen_sans = Playpen_Sans({
+    subsets: ['latin']
+})
+export const lusitana = Lusitana({
+    subsets: ['latin'],
+    weight: ['400', '700']
+})
